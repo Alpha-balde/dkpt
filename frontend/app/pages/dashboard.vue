4b8fa31d8b9f5c2078f5a4d92553630f096c75a5
Alpha Balde <mpbalde2011@gmail.com>
2026-05-18 18:43:20 +0200

    push for [ci:gitlab]

--- frontend/app/pages/dashboard.vue
+++ frontend/app/pages/dashboard.vue
@@ -196,7 +196,7 @@ const participationRate = computed(() => {
           Tableau de bord
         </h1>
         <p class="text-sm text-gray-500 mt-1">
-          Vue d'ensemble des membres, cotisations et paiements. <b>[ci:gitlab]</b>
+          Vue d'ensemble des membres, cotisations et paiements. <b>[ci:gitlab] [ci:bitbucket] [ci:github]</b>
         </p>
       </div>
       <div class="w-32">
